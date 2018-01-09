f84b4b05309392123a2dd6842b093f54a41d5106
Jonny Levy <iamjonnylevy@gmail.com>
2018-01-09 00:24:24 +0000

    Use Main component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Main from './components/Main';
 
 const App = () => (
-  <div>Hello</div>
+  <Provider store={store}>
+    <Main />
+  </Provider>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
