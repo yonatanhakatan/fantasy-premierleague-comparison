f192b1849a19a02a62983cbae488c6f3ab4f5948
Jonny Levy <iamjonnylevy@gmail.com>
2018-01-11 00:23:28 +0000

    Add dummy Card to page

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../redux/actions';
+import Card from './Card';
+import List from './List';
 
 const mapStateToProps = (state) => {
   const {
@@ -16,9 +18,23 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = dispatch => bindActionCreators(actionCreators, dispatch);
 
 const Main = () => (
-  <div>
-    Hello
-  </div>
+  <List>
+    <li>
+      <Card
+        name="Alexis Sanchez"
+        club="Arsenal"
+        position="Defender"
+        imageUrl="https://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/p37265.png"
+        stats={{
+          Price: '£5.4',
+          Form: '3.2',
+          'Transfers In': 111903,
+          'Transfers Out': 202392,
+          'Points Per Game': 3.7,
+        }}
+      />
+    </li>
+  </List>
 );
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
