223c71e8a7fe761de065ccb61f659de42bb1897e
Jonny Levy <iamjonnylevy@gmail.com>
2018-01-13 21:52:53 +0000

    Render Cards from redux store data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import * as actionCreators from '../redux/actions';
 import Card from './Card';
 import List from './List';
 
 const mapStateToProps = (state) => {
   const {
-    card,
+    cards,
   } = state;
 
   return {
-    card,
+    cards,
   };
 };
 
 const mapDispatchToProps = dispatch => bindActionCreators(actionCreators, dispatch);
 
-const Main = () => (
-  <List>
-    <li>
-      <Card
-        name="Alexis Sanchez"
-        club="Arsenal"
-        position="Defender"
-        imageUrl="https://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/p37265.png"
-        stats={{
-          Price: '£5.4',
-          Form: '3.2',
-          'Transfers In': 111903,
-          'Transfers Out': 202392,
-          'Points Per Game': 3.7,
-        }}
-      />
+const CardGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const renderCards = players => (
+  players.map((player, index) => (
+    <li
+      key={player.id}
+    >
+      <CardGroup>
+        {(index > 0) &&
+          <strong>V</strong>
+        }
+        <Card
+          {...player}
+        />
+      </CardGroup>
     </li>
-  </List>
+  ))
 );
+
+const Main = (props) => {
+  const {
+    cards: {
+      players,
+    },
+  } = props;
+
+  if (!players) {
+    return null;
+  }
+
+  return (
+    <List
+      horizontal
+    >
+      {renderCards(players)}
+    </List>
+  );
+};
+
+Main.propTypes = {
+  cards: PropTypes.shape({
+    players: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  }).isRequired,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
