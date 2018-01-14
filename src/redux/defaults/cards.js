7d16cdcdec974ba0727abf4ae1799c165efa6d46
Jonny Levy <iamjonnylevy@gmail.com>
2018-01-14 18:26:00 +0000

    Change default state for Cards to be the placeholders

--- src/redux/defaults/cards.js
+++ src/redux/defaults/cards.js
@@ -1,32 +1,7 @@
-export default {
-  players: [
-    {
-      id: 1,
-      name: 'Alexis Sánchez',
-      club: 'Arsenal',
-      position: 'Forward',
-      imageUrl: 'https://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/p37265.png',
-      stats: {
-        Price: '£11.8',
-        Form: '3.2',
-        'Transfers In': 111903,
-        'Transfers Out': 202392,
-        'Points Per Game': 3.7,
-      },
-    },
-    {
-      id: 394,
-      name: 'Harry Kane',
-      club: 'Tottenham Hostpur',
-      position: 'Forward',
-      imageUrl: 'http://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/p78830.png',
-      stats: {
-        Price: '£12.9',
-        Form: '8.2',
-        'Transfers In': 3711560,
-        'Transfers Out': 2971452,
-        'Points Per Game': 6.7,
-      },
-    },
-  ],
-};
+export default [{
+  id: 1,
+  isPlaceholder: true,
+}, {
+  id: 2,
+  isPlaceholder: true,
+}];
